327b2583b7e71a7010354a71010318fb95d05f88
GalacticQuasar <akashdfw@gmail.com>
2026-04-19 16:55:07 -0500

    feat: about modal github and devpost buttons

--- src/landing.js
+++ src/landing.js
@@ -52,6 +52,16 @@ app.innerHTML = `
           <div class="about-modal-logo-frame">
             <img id="about-modal-logo" src="/room-reveal-logo.png" alt="Room Reveal Logo">
           </div>
+          <div class="about-modal-links">
+            <a href="https://github.com/GalacticQuasar/room-reveal" target="_blank" rel="noopener noreferrer" class="about-modal-link about-modal-link-github">
+              <svg class="about-modal-link-icon" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z" clip-rule="evenodd"></path></svg>
+              GitHub
+            </a>
+            <a href="https://devpost.com/software/room-reveal" target="_blank" rel="noopener noreferrer" class="about-modal-link about-modal-link-devpost">
+              <svg class="about-modal-link-icon" fill="#ffffff" viewBox="0 0 24 24"><path d="M6.002 1.61 0 12.004 6.002 22.39h11.996L24 12.004 17.998 1.61zm1.593 4.084h3.947c3.605 0 6.276 1.695 6.276 6.31 0 4.436-3.21 6.302-6.456 6.302H7.595zm2.517 2.449v7.714h1.241c2.646 0 3.862-1.55 3.862-3.861.009-2.569-1.096-3.853-3.767-3.853z"></path></svg>
+              DevPost
+            </a>
+          </div>
           <p class="about-modal-description">Room Reveal is a platform where students can explore 3D Gaussian Splat models of various living spaces around campus. Models are created when users upload a video of their rooms (after specifying their location and room type) that is transformed into a Gaussian Splat 3D model by our processing pipeline. From there, any user can explore the space interactively on our website to get a sense of potential future living spaces.</p>
           <div class="about-modal-developers">
             <h3>Made By</h3>
